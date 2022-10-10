20d1d824fc4dcfda12dad8d1b36d9b847b366d07
Timothy <tedare115@gmail.com>
2022-10-09 23:17:57 -0700

    3 inputs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import "./App.css";
 import {
+  Header,
   Button,
   Container,
   Dropdown,
@@ -9,11 +10,14 @@ import {
   Segment,
 } from "semantic-ui-react";
 import { useState } from "react";
-import { ReactDatez, ReduxReactDatez } from "react-datez";
+import { ReactDatez } from "react-datez";
 
 function App() {
   const [restaurantIds, setRestaurantIds] = useState([]);
-  const [dateInput, setDateInput] = useState("");
+  const [toDate, setToDate] = useState("");
+  const [fromDate, setFromDate] = useState("");
+  const [fromHour, setFromHour] = useState();
+  const [toHour, setToHour] = useState();
 
   const options = [
     {
@@ -104,46 +108,118 @@ function App() {
     };
   });
 
+  const hourOptions = [
+    { key: 6, text: "6 am", value: 6 },
+    { key: 7, text: "7 am", value: 7 },
+    { key: 8, text: "8 am", value: 8 },
+    { key: 9, text: "9 am", value: 9 },
+    { key: 10, text: "10 am", value: 10 },
+    { key: 11, text: "11 am", value: 11 },
+    { key: 12, text: "12 am", value: 12 },
+    { key: 13, text: "1 pm", value: 13 },
+    { key: 14, text: "2 pm", value: 14 },
+    { key: 15, text: "3 pm", value: 15 },
+    { key: 16, text: "4 pm", value: 16 },
+    { key: 17, text: "5 pm", value: 17 },
+    { key: 18, text: "6 pm", value: 18 },
+    { key: 19, text: "7 pm", value: 19 },
+    { key: 20, text: "8 pm", value: 20 },
+    { key: 21, text: "9 pm", value: 21 },
+    { key: 22, text: "10 pm", value: 22 },
+    { key: 23, text: "11 pm", value: 23 },
+    { key: 24, text: "12 pm", value: 24 },
+    { key: 25, text: "1 am (next day)", value: 25 },
+    { key: 26, text: "2 am (next day)", value: 26 },
+    { key: 27, text: "3 am (next day)", value: 27 },
+    { key: 28, text: "4 am (next day)", value: 28 },
+    { key: 29, text: "5 am (next day)", value: 29 },
+  ];
+
   console.log(restaurantIds);
-  console.log(dateInput);
+  console.log(fromDate);
+  console.log(toDate);
+  console.log(fromHour);
+  console.log(toHour);
 
   return (
     <Container>
-      <Segment>
-        <Dropdown
-          placeholder="Restaurant Ids"
-          fluid
-          multiple
-          selection
-          options={options}
-          value={restaurantIds}
-          onChange={(event, data) => {
-            setRestaurantIds(data.value);
-          }}
-        />
-      </Segment>
+      <div className="landing__title">
+        <h1 className="title">Custom Query Search Tool</h1>
+      </div>
+      <h2 className="landing__subtitle">Select your desired inputs</h2>
       <div className="form-wrapper">
-        <div className="form">
-          <label htmlFor="exampleDate2">From Date</label>
-          <ReactDatez
-            name="dateInput"
-            handleChange={(value) => {
-              setDateInput(value);
+        <div className="restaurant-form">
+        <h4 className="form-title">RESTAURANT</h4>
+          <label>Restaurant Ids</label>
+          <Dropdown
+            fluid
+            multiple
+            selection
+            options={options}
+            value={restaurantIds}
+            onChange={(event, data) => {
+              setRestaurantIds(data.value);
             }}
-            value={dateInput}
           />
         </div>
-        <div className="form">
-          <label htmlFor="exampleDate2">To Date</label>
-          <ReactDatez
-            name="dateInput"
-            handleChange={(value) => {
-              setDateInput(value);
+        <div className="calendar-forms">
+          <div className="form-date">
+          <h4 className="form-title">DATE</h4>
+            <label>From Date</label>
+            <ReactDatez
+              name="fromDate"
+              handleChange={(value) => {
+                setFromDate(value);
+              }}
+              value={fromDate}
+              allowPast={true}
+              dateFormat={"MM/DD/YYYY"}
+              startDate={"2021-01-01"}
+              endDate={"2021-12-31"}
+              defaultMonth={"2021-01-01"}
+            />
+          </div>
+          <div className="form-date">
+            <label>To Date</label>
+            <ReactDatez
+              name="toDate"
+              handleChange={(value) => {
+                setToDate(value);
+              }}
+              value={toDate}
+              allowPast={true}
+              dateFormat={"MM/DD/YYYY"}
+              startDate={"2021-01-01"}
+              endDate={"2021-12-31"}
+              defaultMonth={"2021-01-01"}
+            />
+          </div>
+        </div>
+        <div className="time-forms">
+          <h4 className="form-title">TIME</h4>
+          <label>From Hour</label>
+          <Dropdown
+            fluid
+            selection
+            options={hourOptions}
+            onChange={(event, data) => {
+              setFromHour(data.value);
             }}
-            value={dateInput}
+          />
+          <label>To Hour</label>
+          <Dropdown
+            fluid
+            selection
+            options={hourOptions}
+            onChange={(event, data) => {
+              setToHour(data.value);
+            }}
           />
         </div>
       </div>
+      <div className="metric__title">
+        <h4>METRIC CRITERIA</h4>
+      </div>      
     </Container>
   );
 }
